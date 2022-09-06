6f0fa6dbaf340b282f4d4dd1de0befc2c2e1a5b6
ItsMinhaj <minhajul.islam021@gmail.com>
2022-09-06 20:13:43 +0600

    splash

---
+++ lib/View/splash_screen.dart
@@ -0,0 +1,77 @@
+import 'dart:async';
+
+import 'package:covid_19/View/worlds_current_state.dart';
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late AnimationController controller =
+      AnimationController(vsync: this, duration: const Duration(seconds: 5))
+        ..repeat();
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (_) => const WorldsCurrentState()));
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .25,
+              ),
+              AnimatedBuilder(
+                animation: controller,
+                builder: (context, child) => SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: Center(
+                    child: Transform.rotate(
+                      angle: controller.value * 2 * math.pi,
+                      child: const Image(
+                        image: AssetImage("images/virus.png"),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.08),
+              const Text(
+                "  COVID - 19 \n Tracker App",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white70,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/View/worlds_current_state.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WorldsCurrentState extends StatelessWidget {
+  const WorldsCurrentState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
